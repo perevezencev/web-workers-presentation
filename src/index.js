e33eb6580fd4f098d558f3487ac7cea148638973
Pavel Perevezentsev <perevezenzev@gmail.com>
2019-10-09 11:43:28 +0300

    service worker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import '@tensorflow/tfjs';
 import Stats from 'stats.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 import App from './App';
 
@@ -14,3 +15,4 @@ requestAnimationFrame(function loop(){
 document.body.appendChild(stats.dom);
 
 ReactDOM.render(<App />, document.getElementById('root'));
+serviceWorker.register();
